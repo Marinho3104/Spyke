20e1550a68f027d49495abd613a545be0fb4e075
Marinho3104 <mariovieira2003@gmail.com>
2024-02-02 20:35:37 +0000

    Redesign code

--- tests/transaction_management_gpu/main.cpp
+++ tests/transaction_management_gpu/main.cpp
@@ -1,17 +1,7 @@
 
-#include "./../../transaction_management_gpu/transaction_management_gpu.h"
-#include "./../../transaction_management_gpu/transaction_management_gpu_user_configuration.h"
-
+#include <CL/cl.h>
 
 int main (int argc, char *argv[]) {
-
-  // Config used for the transaction management setup
-  spyke::transaction_management_gpu::Transaction_Management_Gpu_User_Configuration config = 
-    spyke::transaction_management_gpu::Transaction_Management_Gpu_User_Configuration();
-
-  // Gpu config for transactions management
-  spyke::transaction_management_gpu::Transaction_Management_Gpu transaction_management_gpu =
-    spyke::transaction_management_gpu::Transaction_Management_Gpu( config );
 
   return 0;
 
--- transaction_management_gpu/transaction_management_gpu_user_configuration.cpp
+++
@@ -1,8 +0,0 @@
-
-/** INCLUDES **/
-#include "transaction_management_gpu_user_configuration.h"
-
-
-spyke::transaction_management_gpu::Transaction_Management_Gpu_User_Configuration::Transaction_Management_Gpu_User_Configuration() {
-
-}
